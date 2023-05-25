a2abc814b42c2e7f2f9b8eef6b8f239a2fea5bb6
trashmaggot <ilyaskazatov16@mail.ru>
2023-05-25 18:20:24 +0600

    BaseEntity Id type changed to Guid, validators replaced into specified folder

---
+++ WebUI/Validators/ContactInfoValidator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Entities;
+using FluentValidation;
+
+namespace WebUI.Validators;
+
+public class ContactInfoValidator : AbstractValidator<ContactInfo>
+{
+    public ContactInfoValidator()
+    {
+        RuleFor(x => x.Address)
+            .NotEmpty()
+            .Length(1, 50);
+
+        RuleFor(x => x.City)
+            .NotEmpty()
+            .Length(1, 50);
+	
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .Length(1, 100);
+    }
+
+    public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+    {
+        var result = await ValidateAsync(ValidationContext<ContactInfo>.CreateWithOptions((ContactInfo)model, x => x.IncludeProperties(propertyName)));
+        if (result.IsValid)
+            return Array.Empty<string>();
+        return result.Errors.Select(e => e.ErrorMessage);
+    };
+}
---
+++ WebUI/Validators/CreateOrderRequestValidator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Features.OrderFeatures.Commands.CreateOrderCommand;
+using FluentValidation;
+
+namespace WebUI.Validators;
+
+public class CreateOrderRequestValidator : AbstractValidator<CreateOrderRequest>
+{
+    public CreateOrderRequestValidator()
+    {
+        RuleFor(x => x.Weight)
+            .NotEmpty()
+            .InclusiveBetween(1, 1000000);
+
+        RuleFor(x => x.DateTime)
+            .NotEmpty()
+            .LessThanOrEqualTo(date => DateTime.Now);
+
+        RuleFor(x => x.Sender)
+            .SetValidator(new ContactInfoValidator());
+	
+        RuleFor(x => x.Receiver)
+            .SetValidator(new ContactInfoValidator());
+    }
+
+    public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+    {
+        var result = await ValidateAsync(ValidationContext<CreateOrderRequest>.CreateWithOptions((CreateOrderRequest)model, x => x.IncludeProperties(propertyName)));
+        if (result.IsValid)
+            return Array.Empty<string>();
+        return result.Errors.Select(e => e.ErrorMessage);
+    };
+}
